6a5d1ba1bb5ac9cc4ef33e1404961c37b6960f00
John Diginee <Johndbizz@gmail.com>
2023-06-20 18:24:42 +0100

    Bug: Fix deploy script

--- docker/digital_ocean_server_deploy.sh
+++ docker/digital_ocean_server_deploy.sh
@@ -9,9 +9,9 @@ fi
 git archive --format tar --output ./project.tar main
 
 echo 'Uploading project.......:-)...Be Patient!'
-rsync ./project.tar root@DIGITAL_OCEAN_IP_ADDRESS:/tmp/project.tar
+rsync ./project.tar root@$DIGITAL_OCEAN_IP_ADDRESS:/tmp/project.tar
 echo 'Upload complete....:-)'
-ssh -o StrictHostKeyChecking=no root@DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
+ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
     mkdir -p /app
     rm -rf /app/* && tar -xf /tmp/project.tar -C /app
     docker compose -f /app/production.yml up --build -d --remove-orphans
