9cf42802fb75d5292a27ae8bc71b7101136fcafb
John Diginee <Johndbizz@gmail.com>
2023-06-20 18:52:10 +0100

    Bug: Fix deploy script

--- docker/digital_ocean_server_deploy.sh
+++ docker/digital_ocean_server_deploy.sh
@@ -2,18 +2,20 @@
 
 if [ -z "$DIGITAL_OCEAN_IP_ADDRESS" ]
 then
-    echo "DIGITAL_OCEAN_IP_ADDRESS not defined"
-    exit 0
+  echo "DIGITAL_OCEAN_IP_ADDRESS not defined"
+  exit 0
 fi
 
-git archive --format tar --output ./project.tar main
+git archive --format tar --output ./project.tar master
 
 echo 'Uploading project.......:-)...Be Patient!'
 rsync ./project.tar root@$DIGITAL_OCEAN_IP_ADDRESS:/tmp/project.tar
-echo 'Upload complete....:-)'
+echo 'Upload complete...:-)'
+
+echo 'Building the image...'
 ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
-    mkdir -p /app
-    rm -rf /app/* && tar -xf /tmp/project.tar -C /app
-    docker compose -f /app/production.yml up --build -d --remove-orphans
+  mkdir -p /app
+  rm -rf /app/* && tar -xf /tmp/project.tar -C /app
+  docker compose -f /app/production.yml up --build -d --remove-orphans
 ENDSSH
 echo 'Build completed successfully....:-)'
